e4f0c8c4e259016d7276d3d7c4dff8dbc6653f6c
Arthas <something,others@gmail.com>
2024-03-06 14:24:39 +0300

    Scratch Kvadrat

---
+++ Helpers.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int kvadrat(int a, int b)
+{
+	return (a + b)*(a+b);
+}
---
+++ Scratch.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Helpers.h"
+
+
+int main()
+{
+    int a=23;
+    int b=32;
+    int result=kvadrat(a, b);
+    std::cout << "Kvadrat is "<<result<<std::endl;
+}
+
+
